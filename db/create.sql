45f5bff1eec16c6d3c1272482ec4225ade8a1a57
isabellamulder <75012900+isabellamulder@users.noreply.github.com>
2022-03-29 13:41:17 +0000

    spelfoutjes

--- db/create.sql
+++ db/create.sql
@@ -26,6 +26,6 @@ insert into products (name, description, code, jaartal, naam, price) values ('Fr
 insert into products (name, description, code, jaartal, naam, price) values ('Post Mortem', 'Nulla ut erat id mauris vulputate elementum. Nullam varius.', '077030122-3', 1989, 'juliet', 11);
 insert into products (name, description, code, jaartal, naam, price) values ('Scarlet and the Black, The', 'Pellentesque at nulla. Suspendisse potenti.', '445924201-X', 1978, 'esmee', 13.5);
 insert into products (name, description, code, jaartal, naam, price) values ('Aquí llega Condemor, el pecador de la pradera', 'Duis bibendum, felis sed interdum venenatis, turpis enim blandit mi, in porttitor pede justo eu massa.', '693155505-7', 1981, 'narnia', 13.5);
-insert into products (name, description, code, jaaartal, naam, price) values ('Kiss for Corliss, A (Almost a Bride)', 'Nulla ut erat id mauris vulputate elementum. Nullam varius. Nulla facilisi.', '686928463-6', 1974, 'harry', 14);
-insert into products (name, description, code, jaaartal, naam, price) values ('Velvet Goldmine', 'Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla.', '492662523-7', 1986, 'soof', 14);
+insert into products (name, description, code, jaartal, naam, price) values ('Kiss for Corliss, A (Almost a Bride)', 'Nulla ut erat id mauris vulputate elementum. Nullam varius. Nulla facilisi.', '686928463-6', 1974, 'harry', 14);
+insert into products (name, description, code, jaartal, naam, price) values ('Velvet Goldmine', 'Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla.', '492662523-7', 1986, 'soof', 14);
 
